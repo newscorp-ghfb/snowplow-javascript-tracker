6f9d39ced2f74b574f8459d666b1e07e733011aa
Greg Leonard <gregjamesleonard@gmail.com>
2021-10-12 15:06:06 +0100

    Add AllEvents to eventGroups

--- plugins/browser-plugin-youtube-tracking/src/eventGroups.ts
+++ plugins/browser-plugin-youtube-tracking/src/eventGroups.ts
@@ -1,11 +1,9 @@
 import { SnowplowMediaEvent } from './snowplowEvents';
 import { EventGroup } from './types';
 import { YTCustomEvent, YTPlayerEvent, YTStateEvent } from './youtubeEntities';
+import { YTEventName } from './youtubeEvents';
 
-export const AllEvents: string[] = Object.keys(YTStateEvent).concat(
-  Object.keys(YTCustomEvent),
-  Object.keys(SnowplowMediaEvent)
-);
+export const AllEvents: EventGroup = [...Object.keys(YTEventName), ...Object.keys(YTCustomEvent)];
 
 export const DefaultEvents: EventGroup = [
   YTStateEvent.PAUSED,
@@ -17,5 +15,6 @@ export const DefaultEvents: EventGroup = [
 ];
 
 export const EventGroups: { [eventGroup: string]: EventGroup } = {
+  AllEvents: AllEvents,
   DefaultEvents: DefaultEvents,
 };
